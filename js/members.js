ae377a97c2887901c8475afb39ac378d1f8804ce
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2023-08-15 02:34:04 -0400

    added members + best shot button

---
+++ js/members.js
@@ -0,0 +1,57 @@
+function addMemberSection(name, portraitPath, websiteUrl, bestShotUrl) {
+    // Create the section element
+    var section = document.createElement("section");
+    section.className = "member";
+
+    // Create the img element
+    var img = document.createElement("img");
+    img.src = portraitPath;
+    img.alt = name;
+    img.className = "member-portrait";
+    section.appendChild(img);
+
+    // Create the h3 element
+    var h3 = document.createElement("h3");
+    h3.textContent = name;
+    section.appendChild(h3);
+
+    // Create the first link (Website)
+    var websiteLink = document.createElement("a");
+    websiteLink.href = websiteUrl;
+    websiteLink.target = "_blank";
+    websiteLink.textContent = "Website";
+    websiteLink.className = "member-link";
+    section.appendChild(websiteLink);
+
+    // Create the second link (Best shot)
+    var bestShotLink = document.createElement("a");
+    bestShotLink.href = bestShotUrl;
+    bestShotLink.target = "_blank";
+    bestShotLink.textContent = "Best shot";
+    bestShotLink.className = "best-shot-link";
+    section.appendChild(bestShotLink);
+
+    // Append the section to the document body
+    document.body.appendChild(section);
+}
+
+function addMember(name, portraitPath = "", websiteUrl = "", bestShotUrl = "") {
+    var text = `<section class="member">`
+
+    if (portraitPath != "")
+        text += `<img src="` + portraitPath + `" alt="` + name + `" class="member-portrait">`
+
+    text += `<h3>` + name + `</h3>`
+
+    if (websiteUrl != "") {
+        text += `<a href = "` + websiteUrl + `" target = "_blank" class="member-link" > Website</a> `
+    }
+
+    if (bestShotUrl != "") {
+        text += `<a href = "` + bestShotUrl + `" target = "_blank" class="best-shot-link" > Best shot</a> `
+    }
+
+    text += `</section > `
+
+    document.write(text)
+}
--- js/navbar.js
+++ js/navbar.js
@@ -2,8 +2,8 @@ var navbar = `
 <nav>
     <ul>
         <li><a href="about.html">About</a></li>
-        <li><a href="officers.html">Officers</a></li>
         <li><a href="members.html">Members</a></li>
+        <li><a href="officers.html">Officers</a></li>
         <li><a href="merch.html">Merchandise</a></li>
         <li><a href="lore.html">Lore</a></li>
     </ul>
