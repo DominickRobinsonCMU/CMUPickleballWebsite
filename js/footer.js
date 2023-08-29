80c29c7b20e1e747c4cc27816e393e10b3812199
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2023-08-28 20:54:22 -0400

    changed members partition to grad year

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ var footer = `
 <footer>
     <div class="social-media-links">
         <a href="https://www.youtube.com/@cmu_pickleball">YouTube</a>
-        <a href="https://www.instagram.com/cmu_pickleball/">Instagram</a>
+        <a href="https://www.instagram.com/cmu_pickleball">Instagram</a>
         <a href="https://discord.gg/TqazbR8QaJ">Discord</a>
         <a href="https://tartanconnect.cmu.edu/cmupb/home/">Tartan Connect</a>
         <a href="mailto:dominicr@andrew.cmu.edu">Contact us!</a>
--- js/members.js
+++ js/members.js
@@ -1,27 +1,14 @@
-function addMemberSection(name, portraitPath, websiteUrl, bestShotUrl) {
+function addMemberSection(name, bestShotUrl) {
     // Create the section element
     var section = document.createElement("section");
     section.className = "member";
 
-    // Create the img element
-    var img = document.createElement("img");
-    img.src = portraitPath;
-    img.alt = name;
-    img.className = "member-portrait";
-    section.appendChild(img);
 
     // Create the h3 element
     var h3 = document.createElement("h3");
     h3.textContent = name;
     section.appendChild(h3);
 
-    // Create the first link (Website)
-    var websiteLink = document.createElement("a");
-    websiteLink.href = websiteUrl;
-    websiteLink.target = "_blank";
-    websiteLink.textContent = "Website";
-    websiteLink.className = "member-link";
-    section.appendChild(websiteLink);
 
     // Create the second link (Best shot)
     var bestShotLink = document.createElement("a");
@@ -35,18 +22,12 @@ function addMemberSection(name, portraitPath, websiteUrl, bestShotUrl) {
     document.body.appendChild(section);
 }
 
-function addMember(name, portraitPath = "", websiteUrl = "", bestShotUrl = "") {
+function addMember(name, bestShotUrl = "") {
     var text = `<section class="member">`
-
-    if (portraitPath != "")
-        text += `<img src="` + portraitPath + `" alt="` + name + `" class="member-portrait">`
 
     text += `<h3>` + name + `</h3>`
 
-    text += `<a href = "` + websiteUrl + `" target = "_blank" class="member-link"`
-    if (bestShotUrl == "") text += `style="visibility: hidden;"`
-    text += `> Website</a> `
-
+    // if (bestShotUrl == "") text += `style="visibility: hidden;"`
 
     text += `<a href = "` + bestShotUrl + `" target = "_blank" class="best-shot-link" `
     if (bestShotUrl == "") text += `style = "visibility: hidden;"`
