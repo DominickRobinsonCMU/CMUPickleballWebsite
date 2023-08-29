ac04df81739403585afcd0163a4a4d7e89d6b857
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2023-08-28 22:24:33 -0400

    added new members, readded portraits

--- js/members.js
+++ js/members.js
@@ -1,39 +1,18 @@
-function addMemberSection(name, bestShotUrl) {
-    // Create the section element
-    var section = document.createElement("section");
-    section.className = "member";
 
 
-    // Create the h3 element
-    var h3 = document.createElement("h3");
-    h3.textContent = name;
-    section.appendChild(h3);
+function addMember(name, portraitPath = "", bestShotUrl = "") {
+    var text = `<section class="member">`;
 
+    if (portraitPath != "")
+        text += `<img src="images/portraits/` + portraitPath + `" alt="` + name + `" class="member-portrait">`
 
-    // Create the second link (Best shot)
-    var bestShotLink = document.createElement("a");
-    bestShotLink.href = bestShotUrl;
-    bestShotLink.target = "_blank";
-    bestShotLink.textContent = "Best shot";
-    bestShotLink.className = "best-shot-link";
-    section.appendChild(bestShotLink);
-
-    // Append the section to the document body
-    document.body.appendChild(section);
-}
-
-function addMember(name, bestShotUrl = "") {
-    var text = `<section class="member">`
-
-    text += `<h3>` + name + `</h3>`
+    text += `<h3>` + name + `</h3>`;
 
     // if (bestShotUrl == "") text += `style="visibility: hidden;"`
 
-    text += `<a href = "` + bestShotUrl + `" target = "_blank" class="best-shot-link" `
-    if (bestShotUrl == "") text += `style = "visibility: hidden;"`
-    text += `> Best highlight</a> `
+    if (bestShotUrl) text += `<a href = "` + bestShotUrl + `" target = "_blank" class="best-shot-link" > Best highlight</a>`;
 
-    text += `</section> `
+    text += `</section > `;
 
-    document.write(text)
+    document.write(text);
 }
