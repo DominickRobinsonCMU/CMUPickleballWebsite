cf74c69918c0efcce83289db83c34414c7910ed1
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2023-12-31 14:04:48 -0500

    modernized style

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,10 @@
 var navbar = `
 <nav>
-    <ul>
+    <ul class="left-links">
+        <li><img src="images/pickles/pete_the_pickle.png" alt="Logo"></li>
+        <li><a href="about.html">CMU Pickleball</a></li>
+    </ul>
+    <ul class="right-links">
         <li><a href="about.html">About</a></li>
         <li><a href="members.html">Members</a></li>
         <li><a href="officers.html">Officers</a></li>
@@ -13,6 +17,31 @@ var navbar = `
 `;
 
 document.write(navbar);
+
+// Get the height of the navbar including padding
+var navbar = document.querySelector('nav');
+var navbarHeight = window.getComputedStyle(navbar).height;
+console.log('Navbar height including padding:', navbarHeight);
+
+var header = document.querySelector('header');
+header.style.marginTop = navbarHeight;
+
+
+var scrollThreshold = 200; // Example: Change opacity after scrolling 200 pixels
+
+// Event listener for scroll events
+window.addEventListener('scroll', function () {
+    // Get the current scroll position
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Check if the scroll position is below the threshold
+    if (scrollPosition > scrollThreshold) {
+        navbar.style.backgroundColor = 'rgba(51,51,51,0.7)'; // Fully opaque background
+    } else {
+        navbar.style.backgroundColor = 'rgba(51, 51, 51, 1)'; // Restore initial opacity
+    }
+});
+
 
 {/* <li><a href="paddles.html">Paddle Shop</a></li> */ }
 {/* <li><a href="merch.html">Merchandise</a></li> */ }
