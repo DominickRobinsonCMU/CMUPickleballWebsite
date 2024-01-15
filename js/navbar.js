97073e2a71627b18d43329b4ac93a7bb30bf3aed
DominickRobinson <72145105+DominickRobinson@users.noreply.github.com>
2024-01-15 17:19:28 -0500

    tweak navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,20 @@
 var navbar = `
 <nav>
-    <ul class="left-links">
-        <li><img src="images/pickles/pete_the_pickle.png" alt="Logo"></li>
-        <li><a href="about.html">CMU Pickleball</a></li>
-    </ul>
-    <ul class="right-links">
-        <li><a href="members.html">Members</a></li>
-        <li><a href="officers.html">Officers</a></li>
-        <li><a href="articles.html">Articles</a></li>
-        <li><a href="hall-of-fame.html">Hall of Fame</a></li>
-        <li><a href="lore.html">Lore</a></li>
-        <li><a href="links.html">Links</a></li>
-    </ul>
+    <div class="navbar">
+        <a href="about.html">CMU Pickleball</a>
+        <a href="members.html">Members</a>
+        <a href="officers.html">Officers</a>
+        <a href="articles.html">Articles</a>
+        <a href="hall-of-fame.html">Hall of Fame</a>
+        <a href="lore.html">Lore</a>
+        <a href="links.html">Links</a>
+        <a href="calendar.html">Calendar</a>
+    </div>
 </nav>
 `;
+
+{/* <li><img src="images/pickles/pete_the_pickle.png" alt="Logo"></li> */ }
+
 
 document.write(navbar);
 
